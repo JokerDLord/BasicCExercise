4fa007c200e7d6a3b84534c164a0b13cca1711b2
1723663554 <joker.lord@foxmail.com>
2020-02-27 20:56:15 +0800

    end

---
+++ ConsoleApplication1/programstructure.c
@@ -0,0 +1,78 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>//bool类型引用库
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>//字符串操作库
+
+#define PI 3.14159
+
+//static静态本地变量实际是全局变量 但作用域是函数内部
+//全局生存期 本地作用域
+//返回本地变量的地址是危险的
+//返回全局变量、静态本地变量的地址是安全的
+//*使用全局变量和静态本地变量的函数的线程是不安全的
+
+//extern int gall;
+
+//#ifndef __LIST_HEAD__
+//#define __LIST_HEAD__
+
+//#endif
+
+
+//位运算
+int bituse()
+{
+	//按位与& 都为1才得1
+	//按位或| 只要有1就得1
+	//异或 相等为0 不相等为1
+
+	int number;
+	scanf("%d", &number);
+	/*number = 123;*/
+	unsigned mask = 1u << 31;
+	for (; mask; mask >>= 1)
+	{
+		//如果这一位是1，则输出1，否则输出0
+		printf("%d", number & mask ? 1 : 0);
+	}
+	printf("\n");
+	return 0;
+}
+
+
+
+
+
+
+int main()
+{
+	//printf("%0+9d\n", 123);//右对齐有加号填充0
+	//printf("%-9d\n", 123);//左对齐
+	//printf("%9.2f\n", 123.001);//总共9位，小数点取前两位
+	//printf("%d", 123);//左对齐
+
+	//%n用于统计字符数量，放到一个地址&num里面
+
+	//<文件作为输入 >文件作为输出
+
+	/*FILE* fp = fopen("D:\\考研相关\\C练习\\基础练习题\\test1.txt", "r");
+	if (fp)
+	{
+		char line[100];
+		fscanf(fp,"%s", line);
+		printf("%s\n", line);
+		fclose(fp);
+	}
+	else
+	{
+		printf("无法打开文件\n");
+	}*/
+
+
+	bituse();
+	return 0;
+}
--- ResizebalArray/ResizebalArray.c
+++ ResizebalArray/ResizebalArray.c
@@ -1,4 +1,6 @@
-﻿#include <stdio.h>
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
 #include <math.h>
 #include <stdbool.h>//bool类型引用库
 #include <stdlib.h>
@@ -66,24 +68,136 @@ void array_set(Array* a, int index, int value)
 	a->array[index] = value;
 }
 
+//////////////链表定义
+
+typedef struct _node {
+	int value;
+	struct _node* next;//这里不能直接用Node，因为还未出现
+}Node;
+
+typedef struct _list {
+	Node* head;
+	Node* tail;
+}List;
+
+
+//注意传进去的第一个参数所代表的是head的指针，
+//或者用list结构的指针调用head
+void add(List* plist, int number)
+{
+	//add to linked-list
+	Node* p = (Node*)malloc(sizeof(Node));
+	p->value = number;
+	p->next = NULL;
+	//find then last
+	/////////
+	Node* last = plist->head;
+	if (last) {//确保last存在，不是第一个null
+		while (last->next)//只要next存在
+		{
+			last = last->next;
+		}
+		// attach
+		last->next = p;
+	}
+	else {
+		plist->head = p;
+	}
+	/////////以上这一段可以换成用tail做
+
+}
+
+
+//遍历操作
+void print(List* plist)
+{
+	Node* p;
+	for (p = plist->head; p; p = p->next)
+	{
+		printf("%d\t", p->value);
+	}
+	printf("\n");
+}
 
 
 int main()
 {
-	Array a = array_create(100);
+	/*Array a = array_create(100);
 	printf("%d\n", array_size(&a));
 	*array_at(&a, 0) = 10;
 	printf("%d\n", array_get(&a, 0));
-	/*int cnt = 0;
+	int cnt = 0;
 	int number = 0;
 	while (number!=-1)
 	{
-		scanf("%d", &number);
+		scanf_s("%d", &number);
 		if (number != -1)
 			* array_at(&a, cnt++) = number;
-	}*/
+	}
 
-	array_free(&a);
+	array_free(&a);*/
+
+	//链表可更高效的实现可变数组
+	//linked list 
+
+
+	/*Node* head = NULL;*/
+	List list;
+	list.head = NULL;
+	list.tail = NULL;
+	int number;
+	do {
+		scanf("%d", &number);
+		if (number != -1)
+		{
+			add(&list, number);
+		}
+	} while (number != -1);
+	
+	print(&list);
+
+	scanf("%d", &number);
+	Node* p;
+	int isfound = 0;
+	for (p = list.head; p; p = p->next)
+	{
+		if (p->value == number)
+		{
+			printf("找到了\n");
+			isfound = 1;
+			break;
+		}
+	}
+	if (!isfound)
+		printf("没找到\n");
+
+	//删除操作
+	Node* q;
+	for (q=NULL,p = list.head; p; q=p,p = p->next)
+	{
+		if (p->value == number)
+		{
+			if (q) {
+				q->next = p->next;
+				free(p);
+				break;
+			}
+			else {
+				list.head = p->next;
+			}
+		}
+	}
+
+	//链表的清除干净
+	for (p = list.head; p; p = q)
+	{
+		q = p->next;
+		free(p);
+	}
+
+
+	
+
 
 	return 0;
 }
